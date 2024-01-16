abe97dddc1471ae917e2ae620493ebbaf4d6ad30
Madan <codemadan@gmail.com>
2024-01-16 08:54:21 +0545

    [add] pokemon api with fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect( () => {
+        fetch('https://pokeapi.co/api/v2/pokemon')
+            .then( response => response.json())
+            .then( data => setPokemons(data.results))
+    } )
+
+    return (
+        <>
+            <h1>List of Pokemons</h1>
+        </>
+    );
 }
 
 export default App;
